4ae55cf1a89f407ce9d98ac1f3512b95ba2979f4
ulyssesboumann <ulysses@megaleios.com>
2017-09-11 15:29:12 -0300

    noShiftMode bottom navigation bar

--- app/src/main/java/megaleios/com/myseeds/Domains/Main/Activity/MainActivity.java
+++ app/src/main/java/megaleios/com/myseeds/Domains/Main/Activity/MainActivity.java
@@ -1,8 +1,10 @@
 package megaleios.com.myseeds.Domains.Main.Activity;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import megaleios.com.myseeds.Helpers.BottomNavigationViewHelper;
 import megaleios.com.myseeds.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
     }
 }
